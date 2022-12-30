acccdbcd43254fc5fc00d947d9cbc9ee1ca23aab
Terence Boldt <terence@boldt.ca>
2022-12-30 06:05:12 -0500

    Add putall command

--- main.go
+++ main.go
@@ -9,7 +9,6 @@
 package main
 
 import (
-	"encoding/binary"
 	"flag"
 	"fmt"
 	"os"
@@ -18,7 +17,7 @@ import (
 	"github.com/tjboldt/ProDOS-Utilities/prodos"
 )
 
-const version = "0.3.2"
+const version = "0.4.0"
 
 func main() {
 	var fileName string
@@ -33,14 +32,14 @@ func main() {
 	var auxType int
 	flag.StringVar(&fileName, "d", "", "A ProDOS format drive image")
 	flag.StringVar(&pathName, "p", "", "Path name in ProDOS drive image (default is root of volume)")
-	flag.StringVar(&command, "c", "ls", "Command to execute: ls, get, put, rm, mkdir, readblock, writeblock, create")
+	flag.StringVar(&command, "c", "ls", "Command to execute: ls, get, put, rm, mkdir, readblock, writeblock, create, putall")
 	flag.StringVar(&outFileName, "o", "", "Name of file to write")
 	flag.StringVar(&inFileName, "i", "", "Name of file to read")
 	flag.IntVar(&volumeSize, "s", 65535, "Number of blocks to create the volume with (default 65535, 64 to 65535, 0x0040 to 0xFFFF hex input accepted)")
 	flag.StringVar(&volumeName, "v", "NO.NAME", "Specifiy a name for the volume from 1 to 15 characters")
 	flag.IntVar(&blockNumber, "b", 0, "A block number to read/write from 0 to 65535 (0x0000 to 0xFFFF hex input accepted)")
-	flag.IntVar(&fileType, "t", 6, "ProDOS FileType: 0x04 for TXT, 0x06 for BIN, 0xFC for BAS, 0xFF for SYS etc.")
-	flag.IntVar(&auxType, "a", 0x2000, "ProDOS AuxType from 0 to 65535 (0x0000 to 0xFFFF hex input accepted)")
+	flag.IntVar(&fileType, "t", 0, "ProDOS FileType: 0x04 for TXT, 0x06 for BIN, 0xFC for BAS, 0xFF for SYS etc., omit to autodetect")
+	flag.IntVar(&auxType, "a", 0, "ProDOS AuxType from 0 to 65535 (0x0000 to 0xFFFF hex input accepted), omit to autodetect")
 	flag.Parse()
 
 	if len(fileName) == 0 {
@@ -108,49 +107,10 @@ func main() {
 			os.Exit(1)
 		}
 		defer file.Close()
-		if len(pathName) == 0 {
-			fmt.Println("Missing pathname")
-			os.Exit(1)
-		}
-		inFile, err := os.ReadFile(inFileName)
-		if err != nil {
-			fmt.Printf("Failed to open input file %s: %s", inFileName, err)
-			os.Exit(1)
-		}
-		if strings.HasSuffix(strings.ToLower(inFileName), ".bas") {
-			inFile, err = prodos.ConvertTextToBasic(string(inFile))
-			fileType = 0xFC
-			auxType = 0x0801
-		}
 
-		// Check for an AppleSingle file as produced by cc65
-		if // Magic number
-		binary.BigEndian.Uint32(inFile[0x00:]) == 0x00051600 &&
-			// Version number
-			binary.BigEndian.Uint32(inFile[0x04:]) == 0x00020000 &&
-			// Number of entries
-			binary.BigEndian.Uint16(inFile[0x18:]) == 0x0002 &&
-			// Data Fork ID
-			binary.BigEndian.Uint32(inFile[0x1A:]) == 0x00000001 &&
-			// Offset
-			binary.BigEndian.Uint32(inFile[0x1E:]) == 0x0000003A &&
-			// Length
-			binary.BigEndian.Uint32(inFile[0x22:]) == uint32(len(inFile))-0x3A &&
-			// ProDOS File Info ID
-			binary.BigEndian.Uint32(inFile[0x26:]) == 0x0000000B &&
-			// Offset
-			binary.BigEndian.Uint32(inFile[0x2A:]) == 0x00000032 &&
-			// Length
-			binary.BigEndian.Uint32(inFile[0x2E:]) == 0x00000008 {
-
-			fileType = int(binary.BigEndian.Uint16(inFile[0x34:]))
-			auxType = int(binary.BigEndian.Uint32(inFile[0x36:]))
-			inFile = inFile[0x3A:]
-			fmt.Printf("AppleSingle (File type: %02X, AuxType: %04X) detected\n", fileType, auxType)
-		}
-		err = prodos.WriteFile(file, pathName, fileType, auxType, inFile)
+		err = prodos.WriteFileFromFile(file, pathName, fileType, auxType, inFileName)
 		if err != nil {
-			fmt.Printf("Failed to write file %s: %s", pathName, err)
+			fmt.Printf("Failed to write file %s", err)
 		}
 	case "readblock":
 		fmt.Printf("Reading block 0x%04X (%d):\n\n", blockNumber, blockNumber)
@@ -188,6 +148,18 @@ func main() {
 		}
 		defer file.Close()
 		prodos.CreateVolume(file, volumeName, volumeSize)
+	case "putall":
+		file, err := os.Create(fileName)
+		if err != nil {
+			fmt.Printf("failed to create file: %s\n", err)
+			return
+		}
+		defer file.Close()
+		err = prodos.AddFilesFromHostDirectory(file, inFileName, volumeName, volumeSize)
+		if err != nil {
+			fmt.Printf("failed to add host files: %s\n", err)
+			return
+		}
 	case "rm":
 		file, err := os.OpenFile(fileName, os.O_RDWR, 0755)
 		if err != nil {
---
+++ prodos/host.go
@@ -0,0 +1,43 @@
+// Copyright Terence J. Boldt (c)2022
+// Use of this source code is governed by an MIT
+// license that can be found in the LICENSE file.
+
+// This file provides access to generate a ProDOS drive image from a host directory
+
+package prodos
+
+import (
+	"os"
+	"path/filepath"
+)
+
+// AddFilesFromHostDirectory fills the root volume with files
+// from the specified host directory
+func AddFilesFromHostDirectory(
+	readerWriter ReaderWriterAt,
+	directory string,
+	volumeName string,
+	numberOfBlocks int) error {
+	CreateVolume(readerWriter, volumeName, numberOfBlocks)
+
+	files, err := os.ReadDir(directory)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		info, err := file.Info()
+		if err != nil {
+			return err
+		}
+
+		if !file.IsDir() && info.Size() > 0 && info.Size() <= 0x20000 {
+			err = WriteFileFromFile(readerWriter, "", 0, 0, filepath.Join(directory, file.Name()))
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
