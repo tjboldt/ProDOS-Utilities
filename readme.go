c13d1f89304d6f8f35352cb6869b717829128f98
Terence Boldt <terence@boldt.ca>
2024-03-16 13:18:19 -0400

    Update copyright

--- readme.go
+++ readme.go
@@ -14,7 +14,7 @@ func printReadme() {
 	fmt.Println(`
 MIT License
 
-Copyright (c)2021-2023 Terence Boldt
+Copyright (c)2021-2024 Terence Boldt
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
